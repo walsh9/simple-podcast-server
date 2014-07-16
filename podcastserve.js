2a6449d6bbfaa6e72fc7aacf4ba9a6c28fb5720b
Matthew Walsh <walsh9@gmail.com>
2014-07-15 23:24:39 -0400

    Refactor

--- podcastserve.js
+++ podcastserve.js
@@ -6,63 +6,78 @@ var fs = Promise.promisifyAll(require("fs"));
 var path = require('path');
 var id3 = require('id3js');
 
-var wwwPath = "www";
-var mediaExtensions = [".mp3"];
+var PodcastServer = function() {
 
-function isMediaFile(filename) {
-    return _.contains( mediaExtensions, path.extname(filename) )
-}
-
-function getSubDirs(root) {
-    return fs.readdirAsync(root)
-    .map(function getPath(fileName) { return path.join(root, fileName) })
-    .filter(function (filePath) {
-       return fs.statAsync(filePath).then(function(stat) {
-           stat.filePath = filePath;
-           return stat.isDirectory()
-       })
-   }).each( function(x) {path.join(root, x)} )
-}
+    var wwwPath = "www";
+    var mediaExtensions = [".mp3"];
+    var podcastData = {};
 
 
-function getiles(filename) {
-    return true
-}
+    function isMediaFile(filename) {
+        return _.contains(mediaExtensions, path.extname(filename))
+    }
 
+    function getSubDirs(root) {
+        return fs.readdirAsync(root)
+            .map(function getPath(fileName) {
+                return path.join(root, fileName)
+            })
+            .filter(function(filePath) {
+                return fs.statAsync(filePath).then(function(stat) {
+                    stat.filePath = filePath;
+                    return stat.isDirectory()
+                })
+            }).each(function(x) {
+                path.join(root, x)
+            })
+    }
 
-function createPodcast(fileSets) {
-    for (var i = 0, len = fileSets.length; i < len; i++) {
-        var fileSet = fileSets[i];
-        var firstFile = fileSet[0];
-        var dirName = firstFile.split(path.sep)[1]
-        console.log("Creating feed for " + fileSet);
+    function getFiles(folder) {
+        var fileSet = {
+            "folderName": folder
+        }
+        return fs.readdirAsync(folder)
+            .filter(isMediaFile)
+            .then(
+                function(files) {
+                    fileSet["files"] = files;
+                    return fileSet;
+                }
+            )
+    }
+
+    function createFeed(fileSet) {
+        var dirName = fileSet.folderName;
+        var feedTitle = dirName.split(path.sep)[1];
+        console.log("Creating feed for " + dirName);
         var feedOptions = {
-            title: dirName,
-            description: dirName,
+            title: feedTitle,
+            description: feedTitle,
             feed_url: dirName + ".xml"
         };
         var feed = new Podcast(feedOptions);
-        for (var j = 0, len2 = fileSet.length; j < len2; j++) {
-        var fileName = fileSet[j]; 
-            console.log("Creating options for " + fileName);
+        for (var i = 0, len = fileSet.files.length; i < len; i++) {
+            var baseFileName = fileSet.files[i];
+            var fileName = path.join(dirName, baseFileName);
+            console.log("Creating items for " + fileName);
             var itemOptions = {
-                title: path.basename(fileName),
+                title: fileName,
                 description: path.basename(fileName),
-                url:  path.join(path.dirname(fileName) + path.basename(fileName, ".xml")) + ".xml",
+                url: path.join(dirName, path.basename(fileName, path.extname(fileName))) + ".xml",
                 date: Date.now(),
-                enclosure: {url: path.join(path.dirname(fileName) + path.basename(fileName, ".mp3")) + ".xml", file: fileName}
+                enclosure: {
+                    url: path.join(path.basename(dirName, path.extname(fileName))) + ".xml",
+                    file: fileName
+                }
             }
             feed.item(itemOptions);
-        };
-        var xml = feed.xml();
-        var dir = path.dirname(firstFile);
-        fs.writeFile(dir + ".xml", xml);
+            var xml = feed.xml();
+            fs.writeFile(feedTitle + ".xml", xml);
+        }
     }
-}
 
+    getSubDirs(wwwPath)
+        .map(getFiles)
+        .map(createFeed);
 
-getSubDirs( wwwPath )       //read root
-.map(function(filePath) { 
-  return fs.readdirAsync(filePath).map(function(fileName){return path.join(filePath,fileName)}).filter( isMediaFile )
-})    
-.then( createPodcast )
+}();
