9eee9d2871b009a263b5a06b284327a4388078ab
walsh9 <walsh9@gmail.com>
2014-10-18 23:42:45 -0400

    Enable skip buttons

--- js/audio-player.js
+++ js/audio-player.js
@@ -1,17 +1,38 @@
 (function () {
     'use strict';
+    var skipBackInterval = 10;
+    var skipAheadInterval = 30;
     var player = document.querySelector('.player audio');
     var source = document.querySelector('.player audio source');
+    var playButton = document.querySelector('.player .play');
+    var skipBackButton = document.querySelector('.player .skip-back');
+    var skipAheadButton = document.querySelector('.player .skip-ahead');
     var paused = function() {
         return player.paused;
-    }
-    var playButton = document.querySelector('.player .play');
+    };    
     var play = function () {
         player.play();
-    }
+    };
     var pause = function () {
         player.pause();
-    }
+    };
+    var within = function (min, value, max) {
+        return parseInt(Math.min(Math.max(min, value), max));
+    };
+    var skip = function(skipTime) {
+        if (player.duration) {
+            var min = 0;
+            var newTime = player.currentTime + skipTime || 0;
+            var max = player.duration || 0;
+            player.currentTime = within(min, newTime, max);
+        };
+    };
+    var skipBack = function () {
+        skip(-skipBackInterval);
+    };
+    var skipAhead = function () {
+        skip(skipAheadInterval);
+    };
     var toggle = function () {
         if (paused()) {
             playButton.classList.remove('octicon-playback-play');
@@ -37,10 +58,11 @@
         var i, item;
         for (i = 0; i < items.length; i++) {
             item = items[i];
-
             item.addEventListener('click', playItem, true);
         }
         playButton.addEventListener('click', toggle, true);
+        skipBackButton.addEventListener('click', skipBack, true);
+        skipAheadButton.addEventListener('click', skipAhead, true);        
     };
     attachButtons();
 }())
